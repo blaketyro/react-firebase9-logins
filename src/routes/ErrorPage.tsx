01f5707d5f49513e2b8f0c61c1a3d85f4803e32a
blaketyro <blaketyro@gmail.com>
2023-06-26 20:18:29 -0700

    Added navbar

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -1,13 +1,16 @@
+import { Container } from "react-bootstrap";
 import Alert from "react-bootstrap/Alert";
 import { Link } from "react-router-dom";
 
 function ErrorPage() {
 	return (
-		<Alert variant="light">
-			<h3>An error occurred!</h3>
-			<p>Most likely the page you're trying to visit doesn't exist.</p>
-			<Link to="/">Back to homepage</Link>
-		</Alert>
+		<Container className="p-4">
+			<Alert variant="danger">
+				<h3>An error occurred!</h3>
+				<p>Most likely the page you're trying to visit doesn't exist.</p>
+				<Link to="/">Back to homepage</Link>
+			</Alert>
+		</Container>
 	);
 }
 
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,14 +1,34 @@
 import { Outlet, Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Navbar";
 
 function Root() {
 	return (
 		// Renders directly into #root div.
 		<>
-			<p>NAVBAR</p>
-			<Link to="/">Home</Link>
-			<Link to="/login">Login</Link>
-			<Outlet />
-			<p>FOOTER</p>
+			<Navbar className="justify-content-between p-2 flex-wrap">
+				<Nav className="flex-wrap">
+					<Navbar.Brand href="/">React Firebase 9 Logins</Navbar.Brand>
+					<Navbar.Text className="text-warning">Current User: none</Navbar.Text>
+				</Nav>
+				<Nav className="flex-wrap">
+					<Link to="/" className="nav-link">
+						Home
+					</Link>
+					<Link to="/login" className="nav-link">
+						Login
+					</Link>
+					<Link to="/register" className="nav-link">
+						Register
+					</Link>
+					<Nav.Link href="https://github.com/blaketyro/react-firebase9-logins">GitHub</Nav.Link>
+				</Nav>
+			</Navbar>
+			{/* TODO!!! How does the container have nav classes?? */}
+			<Container className="p-4" as="main">
+				<Outlet />
+			</Container>
 		</>
 	);
 }
